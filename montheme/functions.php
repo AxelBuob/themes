02ae57f24d8a874879a115db4517bedeefb84a21
Axel Buob <buobaxel@gmail.com>
2023-11-20 16:18:49 +0100

    Chapter-5-les-actions

--- montheme/functions.php
+++ montheme/functions.php
@@ -1,3 +1,15 @@
 <?php
 
-add_theme_support('title-tag');
+function montheme_supports() {
+    add_theme_support('title-tag');
+};
+
+function montheme_register_assets() {
+    wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css');
+    wp_register_script('bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js", [], false, true);
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_script('bootstrap');
+}
+
+add_action('after_setup_theme', 'montheme_supports');
+add_action('wp_enqueue_scripts', 'montheme_register_assets');
