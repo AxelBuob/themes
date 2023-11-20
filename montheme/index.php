98ead0b7fcb3cb7ffc4be34b000a9eb9d4e313c1
Axel Buob <buobaxel@gmail.com>
2023-11-20 23:14:59 +0100

    Chapter-7-boucle-wordpress

--- montheme/index.php
+++ montheme/index.php
@@ -1,6 +1,20 @@
 <?php get_header() ?>
 
-    <h1>Hello world!</h1>
-    <h2><?= wp_title(); ?></h2>
+<?php if ( have_posts() ): ?>
+    <div class="row">
+        <?php while( have_posts()  ) : the_post(); ?>
+            <div class="col-sm-4">
+                <div class="card" style="width: 18rem;">
+                    <?php the_post_thumbnail('post-thumbnail-small', ['class' => 'card-img-top', 'alt' => 'Card image', 'style' => 'height: auto']); ?>
+                    <div class="card-body">
+                        <h5 class="card-title"><?php the_title()?></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"><?php the_category(); ?></h6>
+                        <p class="card-text"><?php the_content('Lire la suite'); ?></p>
+                    </div>
+                </div>
+            </div>
+        <?php endwhile; ?> 
+    </div>
+<?php endif; ?>
 
 <?php get_footer() ?>
